e453593db7cce4ac21abcdb7969704340973783d
mFahadYounas <fahadyounas97@gmail.com>
2025-10-08 15:26:18 +0500

    Assignment-2 data_validation completed

--- Assignment-2/data_validation.py
+++ Assignment-2/data_validation.py
@@ -1,9 +1,18 @@
+from email_validator import validate_email, EmailNotValidError
+
 def data_type_verifier(val, intype):
     type_map = {
         'integer': int,
         'string': str,
         'float': float,
     }
-    return isinstance(val, type_map[intype])
+    if intype != 'email':
+        return isinstance(val, type_map[intype])
+    else:
+        try:
+            v = validate_email(val, check_deliverability=False)
+            return True
+        except EmailNotValidError:
+            return False
 
-print(data_type_verifier(3, 'float'))
+print(data_type_verifier("your@gmail.com", 'email'))
