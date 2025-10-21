a67b5b74e195d3700772d7ba21d07393a147beb8
mFahadYounas <fahadyounas97@gmail.com>
2025-10-21 18:28:37 +0500

    Refactored word_frequency.py according to better practices

--- assignment_2/word_frequency.py
+++ assignment_2/word_frequency.py
@@ -1,23 +1,25 @@
 import re
 
-def word_freq():
-    freqs = {}
 
-    text = ''
+def word_freq() -> dict:
+    file_name = "assignment_2/assets/my_text.txt"
+    freqs = {}
+    text = ""
     try:
-        with open('my_text.txt') as f:
+        with open(file_name) as f:
             text = f.read()
     except FileNotFoundError as error:
-        print(error)
-    
-    words = [word for word in re.split(r'\W+', text) if word]
-    
+        raise (error)
+
+    words = [word for word in re.split(r"\W+", text) if word]
+
     for word in words:
         if word not in freqs:
             freqs[word] = 1
         else:
             freqs[word] = freqs[word] + 1
-    
+
     return freqs
+
 
 print(word_freq())
