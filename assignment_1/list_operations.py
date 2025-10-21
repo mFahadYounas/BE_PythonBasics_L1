777f6c9aa10dd88b1457978346226a3de794ba04
mFahadYounas <fahadyounas97@gmail.com>
2025-10-21 16:03:47 +0500

    Applied best practices to list operations

--- assignment_1/list_operations.py
+++ assignment_1/list_operations.py
@@ -1,16 +1,34 @@
-def sum_avg_max(inlist):
+from typing import TypedDict
+
+
+class SumAvgMaxOut(TypedDict):
+    sum: int
+    average: float
+    max_in_list: int
+
+
+def sum_avg_max(input_list: list[int]) -> SumAvgMaxOut:
     sum = 0
-    for item in inlist:
+    for item in input_list:
         sum = sum + item
-    average = sum / len(inlist)
-    max1 = max(inlist)
-    print(f"Sum = {sum}\nAverage = {average}\n Max = {max1}")
-    return sum, average, max1
+    average = sum / len(input_list)
+    max_in_list = max(input_list)
+    out: SumAvgMaxOut = {"sum": sum, "average": average, "max_in_list": max_in_list}
+    return out
 
 
-def reverse_list(input_list: list[int]):
-    input_list.reverse()
+def reverse_list(input_list: list[int]) -> list[int]:
+    left_point = 0
+    right_point = len(input_list) - 1
+    temp_for_swap = 0
+    while left_point < right_point:
+        temp_for_swap = input_list[left_point]
+        input_list[left_point] = input_list[right_point]
+        input_list[right_point] = temp_for_swap
+        left_point += 1
+        right_point -= 1
     return input_list
 
 
-print(reverse_list([1, 2, 5]))
+print(sum_avg_max([1, 3, 66]))
+print(reverse_list([1, 2, 5, 12, 0, 12, 5]))
