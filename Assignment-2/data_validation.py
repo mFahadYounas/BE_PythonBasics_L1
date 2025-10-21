83622f1f2d61219c5e75c59f0b67b5275bb6094b
mFahadYounas <fahadyounas97@gmail.com>
2025-10-21 14:16:25 +0500

    Changed variable name

--- Assignment-2/data_validation.py
+++ Assignment-2/data_validation.py
@@ -1,18 +1,20 @@
 from email_validator import validate_email, EmailNotValidError
 
+
 def data_type_verifier(val, intype):
     type_map = {
-        'integer': int,
-        'string': str,
-        'float': float,
+        "integer": int,
+        "string": str,
+        "float": float,
     }
-    if intype != 'email':
+    if intype != "email":
         return isinstance(val, type_map[intype])
     else:
         try:
-            v = validate_email(val, check_deliverability=False)
+            _ = validate_email(val, check_deliverability=False)
             return True
         except EmailNotValidError:
             return False
 
-print(data_type_verifier("your@gmail.com", 'email'))
+
+print(data_type_verifier("your@gmail.com", "email"))
