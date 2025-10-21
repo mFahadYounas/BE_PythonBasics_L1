a9b0c9e8cf90711fd35b52f54449cd815f22759e
mFahadYounas <fahadyounas97@gmail.com>
2025-10-21 18:20:39 +0500

    Added git ignore and refactored address book file handling

--- assignment_2/file_handling.py
+++ assignment_2/file_handling.py
@@ -19,33 +19,43 @@ def read_student_data(filename: str) -> list[float]:
         raise error
 
 
-print(read_student_data("assignment_2/assets/student_data.csv"))
+# print(read_student_data("assignment_2/assets/student_data.csv"))
 
 
-def address_book_in(operation):
-    if operation == "add":
-        name = input("Enter name of new contact: ")
-        phone = input("Enter phone number of new contact: ")
-        if not os.path.exists("Assignment-2/address_book.csv"):
-            with open("Assignment-2/address_book.csv", "a") as f:
-                f.write("Name,Phone\n")
+def address_book_operations(operation: str) -> int:
+    file_name = "assignment_2/assets/address_book.csv"
 
-        with open("Assignment-2/address_book.csv", "a") as f:
-            f.write(f"{name},{phone}\n")
-    elif "search":
-        try:
-            with open("Assignment-2/address_book.csv", "r") as f:
-                reader = csv.reader(f)
-                _ = next(reader)
-                name = input("Enter name to search: ")
-                for row in reader:
-                    if row[0] == name:
-                        print(f"Name = {name}, Phone = {row[1]}")
+    match operation:
+        case "add":
+            name = input("Enter name of new contact: ")
+            phone = input("Enter phone number of new contact: ")
 
-        except FileNotFoundError as error:
-            print(f"{error}: Address book does not exist!")
-    else:
-        print("Invalid operation!")
+            if not os.path.exists(file_name):
+                with open(file_name, "a") as f:
+                    f.write("Name,Phone\n")
+
+            with open(file_name, "a") as f:
+                f.write(f"{name},{phone}\n")
+
+            return 1
+
+        case "search":
+            try:
+                with open(file_name, "r") as f:
+                    reader = csv.reader(f)
+                    _ = next(reader)
+                    name = input("Enter name to search: ")
+                    for row in reader:
+                        if row[0] == name:
+                            print(f"Name = {name}, Phone = {row[1]}")
+
+            except FileNotFoundError as error:
+                print(f"{error}: Address book does not exist!")
+
+            return 1
+
+        case _:
+            raise ValueError("Invalid Operation!")
 
 
-# address_book_in("search")
+address_book_operations("add")
