414aa81b3e010fddb3e8e03c1b26b9a517b623a4
mFahadYounas <fahadyounas97@gmail.com>
2025-10-21 17:22:17 +0500

    Fixed a few edge cases in fib seq

--- assignment_2/Fibonacci_sequence.py
+++ assignment_2/Fibonacci_sequence.py
@@ -1,10 +1,16 @@
-def fib_seq(n: int) -> list[int]:
+def fib_seq(seq_len: int) -> list[int]:
+    if seq_len == 1:
+        return [0]
+    if seq_len <= 0:
+        return []
+
     output = [0, 1]
     i = 2
-    while i < n:
+    while i < seq_len:
         output.append(output[i - 1] + output[i - 2])
         i += 1
+
     return output
 
 
-print(fib_seq(10))
+print(fib_seq(3))
