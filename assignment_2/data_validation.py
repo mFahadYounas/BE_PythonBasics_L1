6c324ad7cf46114d4df5bb7ab110b0d32bf8315d
mFahadYounas <fahadyounas97@gmail.com>
2025-10-21 17:04:42 +0500

    Added type annotations to data type verifier and removed unnecessary else statement

--- assignment_2/data_validation.py
+++ assignment_2/data_validation.py
@@ -1,20 +1,19 @@
 from email_validator import validate_email, EmailNotValidError
 
 
-def data_type_verifier(val, intype):
+def data_type_verifier(val, input_type: str) -> bool:
     type_map = {
         "integer": int,
         "string": str,
         "float": float,
     }
-    if intype != "email":
-        return isinstance(val, type_map[intype])
-    else:
-        try:
-            _ = validate_email(val, check_deliverability=False)
-            return True
-        except EmailNotValidError:
-            return False
+    if input_type != "email":
+        return isinstance(val, type_map[input_type])
+    try:
+        _ = validate_email(val, check_deliverability=False)
+        return True
+    except EmailNotValidError:
+        return False
 
 
 print(data_type_verifier("your@gmail.com", "email"))
