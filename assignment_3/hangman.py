f578ba4918d2a6268a302ce535009f1331873acf
mFahadYounas <fahadyounas97@gmail.com>
2025-10-21 18:56:47 +0500

    Minor refactoring of assignment_3

--- assignment_3/hangman.py
+++ assignment_3/hangman.py
@@ -1,6 +1,6 @@
 import random
 
-words = [
+words: list[str] = [
     "serendipity",
     "ephemeral",
     "ubiquitous",
@@ -23,45 +23,48 @@ words = [
     "magnanimous",
 ]
 
-def select_word(words):
+
+def select_word(words: list[str]) -> str:
     index = random.randrange(0, len(words))
     return words[index]
 
-def find_char(c, word):
-    found_idx = []
-    for i in range(len(word)):
-        if word[i] == c:
-            found_idx.append(i)
-    return found_idx
+
+def find_char(c: str, word: str) -> list[int]:
+    return [i for i in range(len(word)) if word[i] == c]
 
 
-def game():
+def game() -> int:
     selected_word = select_word(words)
     len_selected_word = len(selected_word)
-    hidden_word = '_' * len_selected_word
+    hidden_word = ["_" for _ in selected_word]
     print(hidden_word)
-    hidden_word = list(hidden_word)
 
     allowed_guesses = 5
     correct_guess = 0
 
     while correct_guess < len_selected_word and allowed_guesses > 0:
-        gc = input("Guess a letter: ")
-        found_idx = find_char(gc, selected_word)
-        if(found_idx):
+        guessed_char = input("Guess a letter: ")
+        found_idx = find_char(guessed_char, selected_word)
+
+        if found_idx:
             for i in found_idx:
-                hidden_word[i] = gc
+                hidden_word[i] = guessed_char
                 correct_guess += 1
             print(hidden_word)
-        else:
-            allowed_guesses -= 1
-            print(hidden_word)
-            print(f"Guesses remaining = {allowed_guesses}")
+            continue
 
-    if(correct_guess == len_selected_word):
+        allowed_guesses -= 1
+        print(hidden_word)
+        print(f"Guesses remaining = {allowed_guesses}")
+
+    if correct_guess == len_selected_word:
         print("You Win!")
-        return
-    if(allowed_guesses == 0):
-        print("You Lose!")
+        return 1
+
+    if allowed_guesses == 0:
+        print(f"You Lose! Word was {selected_word}")
+
+    return 1
+
 
 game()
